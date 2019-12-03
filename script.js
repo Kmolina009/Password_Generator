5e9d4ba4906a89a32c4b7d80138b27c1f14ef188
Keven Molina <kevenmolina@gmail.com>
2019-12-03 16:15:57 -0600

    functionality for password created, style added)

--- script.js
+++ script.js
@@ -10,13 +10,30 @@ const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const clipboardEl = document.getElementById('clipboard');
 
-
+//Store generator functions within object
 const randomFunc = {
     lower: getRandomLower,
     upper:getRandomUpper,
     number: getRandomNumber,
     symbol: getRandomSymbol
 }
+
+//Clipboard function
+clipboardEl.addEventListener('click',() => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerHTML
+
+    if(!password){
+        return ''
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);;
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard')
+})
 
 generateEl.addEventListener('click', () => {
     const length = lengthEl.value;
@@ -35,25 +52,36 @@ generateEl.addEventListener('click', () => {
 })
 //Generate password function
 
-function generatePassword(lower,upper, number,symbol, length){
+function generatePassword(lower, upper, number, symbol, length){
     //1. Init pw var
     let generatedPassword = '';
     //2. Filter out unchecked types
         //count the number of checked items
-        const typesCount = lower + upper + number+ symbol;
+        const typesCount = lower + upper + number + symbol;
         console.log('typesCount:' + typesCount)
         
         //make the parameters objects with boolean values
         const typesArr = [{ lower }, { upper },{ number }, { symbol}].filter(
-            //filter out falsey values
-            item => Object.value(item)[0]
+            //filter out falsey values(HO's take higer order values in plural form)
+            item => Object.values(item)[0]
         );
 
-        console.log('typesArr: ', typeArr)
-    //3. Loop over length call generator function for each type
-            for(let i = 0; i <= length.value; i++){
+        console.log('typesArr: ', typesArr)
 
-            }
+        if(typesCount === 0){
+            return '';
+        }
+        //as long as i is less than or equal to length, add to type count.
+        for(let i = 0; i <= length; i += typesCount){
+            typesArr.forEach(type => {
+                const funcName = Object.keys(type)[0]
+                
+                generatedPassword += randomFunc[funcName]();
+            });
+        }
+        const finalPassword = generatedPassword.slice(0, length);
+        return finalPassword
+    //3. Loop over length call generator function for each type
     //4. Add final pw to the pw var and return
 }
 
@@ -82,3 +110,5 @@ function getRandomSymbol(){
 // console.log(getRandomUpper())
 // console.log(getRandomNumber())
 // console.log(getRandomSymbol())
+
+//Animations
